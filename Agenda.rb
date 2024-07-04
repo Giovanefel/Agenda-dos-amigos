64e931551633c3dd8dca5701b5d1f13425f0806a
giovanefel <giovanefe17@gmail.com>
2024-07-04 00:21:23 -0300

    cont9:40m

--- Agenda.rb
+++ Agenda.rb
@@ -14,10 +14,10 @@
 
 @agenda =[
 {nome:"EuGiovane" , telefone:"21971331981"},
-{nome:"Amigo1" , telefone:"1"},
-{nome:"Amigo2" , telefone:"2"},
-{nome:"Amigo3" , telefone:"3"},
-{nome:"Amigo4" , telefone:"4"},
+#{nome:"Amigo1" , telefone:"1"},
+#{nome:"Amigo2" , telefone:"2"},
+#{nome:"Amigo3" , telefone:"3"},
+#{nome:"Amigo4" , telefone:"4"},
 ]
 def todos_contatos
     @agenda.each do |contato|
@@ -25,17 +25,36 @@ def todos_contatos
     end
 end
 
+def adicionar_contato
+    print "Nome:"
+    nome = gets.chomp
+    print "telefone:"
+    telefone = gets.chomp
+
+    @agenda << {nome: nome, telefone: telefone}
+
+end
+
 loop do 
 
-puts " 1. Contatos\n2. Adicionar Contato\n3. Ver Contato\n4. Editar Contato\n5. Remover Contato\n0. Sair"
-    codigo= gets.chomp.to_i
-    case 
+     puts "1. Contatos\n2. Adicionar Contato\n3. Ver Contato\n4. Editar Contato\n5. Remover Contato\n0. Sair"
+    codigo = gets.chomp.to_i
+
+    case codigo
     when codigo == 0
         puts"Até logo"
-        break
-
-    when contato == 1
-    end
+    
+    break
+    when codigo == 1
+        todos_contatos
+        puts"lista de contatos"
+    
+    when codigo == 2
+    adicionar_contato
+    puts"dados do contato"
+    
+        
+     end
 end
 # faça um teste para verificar se rodou 
     # assim que alterar algo infomar no grupo
